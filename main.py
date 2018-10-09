27bb094a253ea64ed1294e679db160959b04eb35
Julian Hung <hungsjulian@gmail.com>
2018-10-08 19:57:46 -0500

    finished the basics

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ db = SQLAlchemy(app)
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    body = db.Column(db.String(500))
+    body = db.Column(db.String(1000))
 
     def __init__(self, title, body):
         self.title = title
@@ -18,15 +18,36 @@ class Blog(db.Model):
 
 @app.route('/blog', methods=['GET', 'POST'])
 def index():
+    id = request.args.get("id")
+    if id:
+        post = Blog.query.filter_by(id=id).first()
+        return render_template('post.html', title=post.title, body=post.body)
+
     posts = Blog.query.all()
     return render_template('blog.html', posts = posts)
 
 @app.route('/newpost', methods=['GET', 'POST'])
 def add_post():
     if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        title_error = True
+        body_error = True
+        if title:
+            title_error = False
+        if body:
+            body_error = False
 
-    else:
-        return render_template("newpost.html")
+        if title_error or body_error:
+            return render_template("newpost.html", title=title, body=body, title_error=title_error, body_error=body_error)
+        else:
+            new_post = Blog(title, body)
+            db.session.add(new_post)
+            db.session.commit()
+            return redirect("/blog?id={0}".format(new_post.id))
+        
+
+    return render_template("newpost.html")
 
 if __name__ == "__main__":
     app.run()
